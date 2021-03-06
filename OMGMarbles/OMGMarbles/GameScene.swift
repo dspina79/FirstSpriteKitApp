c293d6d452dede7a4531f7e6dbac2ef9fa01fef7
Dave Spina <dspina79@gmail.com>
2021-03-05 20:53:59 -0500

    Added ball array

--- OMGMarbles/OMGMarbles/GameScene.swift
+++ OMGMarbles/OMGMarbles/GameScene.swift
@@ -10,9 +10,28 @@ import SpriteKit
 class Ball: SKSpriteNode {}
 
 class GameScene: SKScene {
-    var balls = ["ballBlue", "ballGreen", "ballCyan", "ballGray", "ballPurple", "ballRed", "ballYellow"]
+    var balls = ["ballBlue", "ballGreen", "ballCyan", "ballGrey", "ballPurple", "ballRed", "ballYellow"]
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "checkerboard")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.alpha = 0.2
+        background.zPosition = -1
+        
+        addChild(background)
+        let ball = SKSpriteNode(imageNamed: "ballBlue")
+        let ballRadius = ball.frame.width / 2.0
+        
+        for i in stride(from: ballRadius, through: view.bounds.width - ballRadius, by: ball.frame.width) {
+            for j in stride(from: 100, through: view.bounds.height - ballRadius, by: ball.frame.height) {
+                let ballType = balls.randomElement()!
+                let ball = Ball(imageNamed: ballType)
+                ball.position = CGPoint(x: i, y: j)
+                ball.name = ballType
+                
+                addChild(ball)
+            }
+        }
 
     }
     
